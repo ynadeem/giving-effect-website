e21d6092ef56a672ad130418ce450760d47a6113
Yasser Nadeem <ok.yasser@gmail.com>
2022-02-06 01:57:01 -0500

    Adds mobile styling

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -25,33 +25,40 @@ export const BlogPostTemplate = ({
   return (
     <div className="relative">
       {helmet || ""}
-      <div>
+      <div className="md:flex hidden">
         <img src={imageBanner} className="max-w-full"></img>
       </div>
-      <div className="relative flex justify-center">
-        <div className="max-w-5xl rounded shadow-lg bg-white absolute -top-40">
-          <div className="w-5/6 mx-auto">
-            <div className="flex text-6xl text-primary-green-dark justify-center pt-8">
+      <div className="md:relative flex justify-center">
+        <div className="md:max-w-5xl max-w-full rounded shadow-xl bg-white md:absolute md:-top-40 md:px-0 px-4">
+          <div className="md:w-5/6 mx-auto">
+            <div className="flex md:text-6xl text-2xl text-primary-green-dark justify-center pt-8">
               {title}
             </div>
-            <div className="text-md text-gray-400 mb-2">{publishDate}</div>
-            <div className="flex justify-center text-lg mb-6">
+            <div className="flex text-md text-gray-400 mb-2 md:justify-start justify-center md:text-xl text-sm">
+              {publishDate}
+            </div>
+            <div className="flex justify-center md:text-lg text-sm mb-6">
               {description}
             </div>
             <img
               src={featuredImage}
               alt="Featured Image"
-              className="max-w-3xl mx-auto mb-6"
+              className="md:max-w-3xl max-w-md mx-auto mb-6"
             ></img>
-            <PostContent content={content} className="py-2 text-justify" />
+            <PostContent
+              content={content}
+              className="py-2 text-justify md:text-lg text-sm"
+            />
             {tags && tags.length ? (
               <div className="mt-4">
-                <div className="text-lg">Tags</div>
+                <div className="md:text-lg text-md flex md:justify-start justify-center">
+                  Tags:
+                </div>
                 <div className="">
                   {tags.map((tag) => (
                     <div
                       key={tag + `tag`}
-                      className="text-primary-green-dark hover:text-primary-green-light"
+                      className="md:text-lg text-sm text-primary-green-dark hover:text-primary-green-light hover:underline flex md:justify-start justify-center"
                     >
                       <Link to={`/tags/${kebabCase(tag)}/`}>{tag}</Link>
                     </div>
