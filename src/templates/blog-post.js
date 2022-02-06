bf57d5eb19d1a7fda44d8614ceec25bf1266616a
Yasser Nadeem <ok.yasser@gmail.com>
2022-02-06 00:27:21 -0500

    Add light green banner and begin styling

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -6,56 +6,67 @@ import Layout from "../components/Layout";
 import React from "react";
 import { kebabCase } from "lodash";
 
+// import { lightGreenBanner } from "../../static/img/light-green-banner.png";
+
 // eslint-disable-next-line
 export const BlogPostTemplate = ({
   content,
   contentComponent,
   description,
   featuredImage,
+  imageBanner,
   tags,
   title,
   helmet,
 }) => {
   const PostContent = contentComponent || Content;
 
-  
+  console.log(imageBanner);
 
   return (
-    <section className="">
+    <div className="relative">
       {helmet || ""}
-      <div className="max-w-6xl rounded shadow-lg bg-white mx-auto">
-        <img src={featuredImage}>
-        </img>
-        <div className="">
-          <div className="">
-            <h1 className="text-4xl text-primary-green-dark px-12 py-2">
+      <div>
+        <img src={imageBanner} className="max-w-full"></img>
+      </div>
+      <div className="relative flex justify-center">
+        <div className="max-w-5xl rounded shadow-lg bg-white absolute -top-40">
+          <div>
+            <div className="flex text-5xl text-primary-green-dark justify-center pt-8 mb-2">
               {title}
-            </h1>
-            <p className="text-2xl px-12 py-2">{description}</p>
-            <PostContent content={content} className="px-12 py-2" />
-            {tags && tags.length ? (
-              <div className="mt-4">
-                <h4>Tags</h4>
-                <ul className="">
-                  {tags.map((tag) => (
-                    <li key={tag + `tag`}>
-                      <Link to={`/tags/${kebabCase(tag)}/`}>{tag}</Link>
-                    </li>
-                  ))}
-                </ul>
+            </div>
+            <div>
+              <div className="flex justify-center text-lg mb-6 px-20">
+                {description}
               </div>
-            ) : null}
+              <img
+                src={featuredImage}
+                alt="Featured Image"
+                className="max-w-3xl mx-auto mb-6"
+              ></img>
+              <PostContent content={content} className="px-12 py-2" />
+              {tags && tags.length ? (
+                <div className="mt-4">
+                  <h4>Tags</h4>
+                  <ul className="">
+                    {tags.map((tag) => (
+                      <li key={tag + `tag`}>
+                        <Link to={`/tags/${kebabCase(tag)}/`}>{tag}</Link>
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+              ) : null}
+            </div>
           </div>
         </div>
       </div>
-    </section>
+    </div>
   );
 };
 
-
 const BlogPost = ({ data }) => {
-  const { markdownRemark: post } = data;
-  console.log(post.frontmatter.featuredimage.publicURL)
+  const { markdownRemark: post, exampleImage: imageBanner } = data;
 
   return (
     <Layout>
@@ -64,6 +75,7 @@ const BlogPost = ({ data }) => {
         contentComponent={HTMLContent}
         description={post.frontmatter.description}
         featuredImage={post.frontmatter.featuredimage.publicURL}
+        imageBanner={imageBanner.publicURL}
         helmet={
           <Helmet titleTemplate="%s | Blog">
             <title>{`${post.frontmatter.title}`}</title>
@@ -80,7 +92,6 @@ const BlogPost = ({ data }) => {
   );
 };
 
-
 export default BlogPost;
 
 export const pageQuery = graphql`
@@ -94,9 +105,29 @@ export const pageQuery = graphql`
         description
         featuredimage {
           publicURL
-          }
+        }
         tags
       }
     }
+
+    exampleImage: file(
+      extension: { eq: "png" }
+      name: { eq: "light-green-banner" }
+    ) {
+      publicURL
+    }
   }
 `;
+
+// query SpecificImageQuery {
+// 	exampleImage: file(
+// 		extension: {eq: "png"},
+//     name: {eq: "light-green-banner"}
+//   ) {
+// 	  childImageSharp {
+//       fluid {
+//         src
+//       }
+//     }
+// 	}
+// }
